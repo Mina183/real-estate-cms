a15e75ce7d2e7cfd96103448e1d7d02d9bf5c513
Mina <minakovacevic@ymail.com>
2025-07-14 16:27:59 +0200

    Add command to delete superadmin

---
+++ app/Console/Commands/DeleteSuperadmin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DeleteSuperadmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-superadmin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+public function handle()
+{
+    $email = 'mk@poseidonhumancapital.com'; // <-- replace with actual superadmin email
+    $user = \App\Models\User::where('email', $email)->first();
+
+    if ($user) {
+        $user->delete();
+        $this->info("Superadmin with email {$email} deleted.");
+    } else {
+        $this->error("User with email {$email} not found.");
+    }
+
+    return 0;
+}
+}
