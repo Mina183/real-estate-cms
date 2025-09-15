de3c0c4397547b55fc2030a7985645936f381ca2
Mina <minakovacevic@ymail.com>
2025-09-15 13:54:26 +0200

    fix demo video embed URL issue

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-4xl mx-auto">
-            <iframe width="100%" height="500" src="https://youtu.be/krN3e5uILH0" frameborder="0" allowfullscreen></iframe>
+            <iframe width="100%" height="500" src="https://www.youtube.com/embed/krN3e5uILH0" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
 </x-app-layout>
