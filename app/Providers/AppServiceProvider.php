0f405ea890df1cb710c94a976e43e14ca681c4f2
Mina <minakovacevic@ymail.com>
2026-02-25 23:33:04 +0100

    add password policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,30 +7,34 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-public function boot(): void
-{
-    $logPath = storage_path('logs/laravel.log');
+    public function boot(): void
+    {
+        $logPath = storage_path('logs/laravel.log');
 
-    if (!file_exists(dirname($logPath))) {
-        mkdir(dirname($logPath), 0755, true);
-    }
+        if (!file_exists(dirname($logPath))) {
+            mkdir(dirname($logPath), 0755, true);
+        }
 
-    if (!file_exists($logPath)) {
-        file_put_contents($logPath, '');
+        if (!file_exists($logPath)) {
+            file_put_contents($logPath, '');
+        }
+
+        Password::defaults(function () {
+            return Password::min(12)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised();
+        });
     }
-}
 }
