f1f7f7c51bb35312a0a65bb4c802f41cc6b9a2f8
Mina <minakovacevic@ymail.com>
2026-04-07 07:49:42 +0200

    add favicon to blades

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Triton Platform</title>
+    <link rel="icon" type="image/png" href="/images/logo.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gradient-to-br from-brand-accent-light/20 to-brand-light/30 text-brand-darker min-h-screen flex flex-col lg:flex-row">
